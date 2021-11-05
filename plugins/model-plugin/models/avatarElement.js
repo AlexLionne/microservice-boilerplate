328f8dfc0036702e42db89195464aeac5eda0623
AlexLionne <alexandrelionne27@gmail.com>
2021-11-05 22:02:55 +0100

    Update avatarElement.js

--- plugins/model-plugin/models/avatarElement.js
+++ plugins/model-plugin/models/avatarElement.js
@@ -5,9 +5,8 @@ const {Storage} = require('@google-cloud/storage');
 const {config} = require("../config/knex");
 
 const guid = require('objection-guid')({
-  field: 'avatarElementId',
+    field: 'avatarElementId',
 });
-
 
 
 const storage = new Storage();
@@ -16,50 +15,54 @@ const storage = new Storage();
 Model.knex(config)
 
 class Avatar extends guid(Model) {
-  static get tableName() {
-    return 'avatarElement';
-  }
-
-  static get idColumn() {
-    return 'avatarElementId';
-  }
-
-
-  async $afterFind(args) {
-
-    this.preview = null
-
-    const file = await storage
-        .bucket(AVATARS_BUCKET)
-        .file(`${this.type}/${this.name}.png`)
-
-    const [exists] = await file.exists()
-
-    if (exists) {
-      const [preview] = await storage
-          .bucket(AVATARS_BUCKET)
-          .file(`${this.type}/${this.name}.png`)
-          .getSignedUrl({
-            version: 'v4',
-            action: 'read',
-            expires: Date.now() + BUCKET_RESOURCE_OPTIONS, // 3 hours
-          })
-
-      this.preview = preview
+    static get tableName() {
+        return 'avatarElement';
     }
-  }
 
-  static get jsonSchema() {
-    return {
-      type: 'object',
-      properties: {
-        avatarElementId: {type: 'string'},
-        name: {type: 'string'},
-        avatarSkinId: {type: 'string'},
-        type: {type: 'string'},
-      }
-    };
-  }
+    static get idColumn() {
+        return 'avatarElementId';
+    }
+
+
+    async $afterFind(args) {
+
+        if (this.canPreview) {
+
+            this.preview = null
+
+            const file = await storage
+                .bucket(AVATARS_BUCKET)
+                .file(`${this.type}/${this.name}.png`)
+
+            const [exists] = await file.exists()
+
+            if (exists) {
+                const [preview] = await storage
+                    .bucket(AVATARS_BUCKET)
+                    .file(`${this.type}/${this.name}.png`)
+                    .getSignedUrl({
+                        version: 'v4',
+                        action: 'read',
+                        expires: Date.now() + BUCKET_RESOURCE_OPTIONS, // 3 hours
+                    })
+
+                this.preview = preview
+            }
+        }
+
+    }
+
+    static get jsonSchema() {
+        return {
+            type: 'object',
+            properties: {
+                avatarElementId: {type: 'string'},
+                name: {type: 'string'},
+                avatarSkinId: {type: 'string'},
+                type: {type: 'string'},
+            }
+        };
+    }
 }
 
 module.exports = Avatar
