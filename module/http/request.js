11902e773f7ec862061d4a77a8538ead298849a5
Alex Lionne <alexandrelionne27@gmail.com>
2021-12-24 17:16:25 +0100

    rmv

--- module/http/request.js
+++ module/http/request.js
@@ -33,13 +33,10 @@ module.exports = function request(microservice, handler, plugins, route, log, di
     }
 
     try {
-
         if (method.toLowerCase() === 'get') microservice.get(endpoint, middleware)
         if (method.toLowerCase() === 'post') microservice.post(endpoint, middleware)
         if (method.toLowerCase() === 'put') microservice.put(endpoint, middleware)
         if (method.toLowerCase() === 'delete') microservice.delete(endpoint, middleware)
-        if (method.toLowerCase() === '*') microservice[method.toLowerCase()](endpoint, middleware)
-
     } catch (e) {
         return e.message
     }
