6690394b4cbda25822e207ae09700782900d0f5e
Alex Lionne <alexandrelionne27@gmail.com>
2022-01-09 12:01:58 +0100

    local ip

--- module/socket-client.js
+++ module/socket-client.js
@@ -1,6 +1,6 @@
 const io = require('socket.io-client')
 
-const SOCKET_URL = process.env.ENV === 'DEV' ? 'ws://nesga-socket:3800': 'ws://api.nesga.fr/socket'
+const SOCKET_URL = process.env.ENV === 'DEV' ? 'ws://0.0.0.0:3800': 'ws://api.nesga.fr/socket'
 
 function socket() {
     return io(SOCKET_URL, {
