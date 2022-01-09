7e2d108687d7f7b0fcbf25b66772295af9244649
Alex Lionne <alexandrelionne27@gmail.com>
2022-01-09 19:59:28 +0100

    update socket client

--- module/socket-client.js
+++ module/socket-client.js
@@ -1,6 +1,6 @@
 const io = require('socket.io-client/dist/socket.io')
 
-const SOCKET_URL = process.env.ENV === 'DEV' ? 'ws://0.0.0.0:3800': 'ws://api.nesga.fr/socket'
+const SOCKET_URL = process.env.ENV === 'DEV' ? 'ws://nesga-socket:3800' : 'ws://api.nesga.fr/socket'
 
 module.exports.socket = () => io(SOCKET_URL, {
         jsonp: false,
