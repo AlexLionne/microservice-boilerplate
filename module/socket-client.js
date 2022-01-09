3bb3fd306e4c9c2a2d0e8a7d5067946501ebec6c
Alex Lionne <alexandrelionne27@gmail.com>
2022-01-09 20:08:41 +0100

    test

--- module/socket-client.js
+++ module/socket-client.js
@@ -1,9 +1,12 @@
-const io = require('socket.io-client/dist/socket.io')
+const {io} = require('socket.io-client')
 
 const SOCKET_URL = process.env.ENV === 'DEV' ? 'ws://nesga-socket:3800' : 'ws://api.nesga.fr/socket'
 
-module.exports.socket = () => io(SOCKET_URL, {
+module.exports.socket = () => {
+
+    return io(SOCKET_URL, {
         jsonp: false,
         transports: ['websocket']
     });
+}
 
