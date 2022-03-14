2fc950d3d8235518180f686d3a334e76718b008d
Alex Lionne <alexandrelionne27@gmail.com>
2022-03-14 19:38:52 +0100

    feat(user): update user model

--- plugins/model-plugin/models/user.js
+++ plugins/model-plugin/models/user.js
@@ -1,5 +1,6 @@
 const {Model} = require('objection');
 const {config} = require("../config/knex");
+const UserClub = require("./userClub");
 
 const guid = require('objection-guid')({
     field: 'userId',
@@ -27,21 +28,37 @@ class User extends guid(Model) {
                 authId: {type: 'string'},
                 firstConnectionDate: {type: 'string'},
                 lastConnectionDate: {type: 'string'},
+                timezone: {type: 'string'},
+                FCMToken:  {type: 'string'},
+                googleId: {type: 'string'},
+                appleId: {type: 'string'},
+                facebookId: {type: 'string'},
                 active: {type: 'tinyint'},
+                ban: {type: 'tinyint'},
+                admin: {type: 'tinyint'},
             }
         };
     }
     static get relationMappings() {
         const UserClub = require('./userClub');
         const Device = require('./device');
+        const UserData = require('./userData');
 
         return {
-            userClub: {
+            club: {
                 relation: Model.HasOneRelation,
                 modelClass: UserClub,
                 join: {
                     from: 'user.userId',
                     to: 'userClub.userId'
+                }
+            },
+            data: {
+                relation: Model.HasOneRelation,
+                modelClass: UserData,
+                join: {
+                    from: 'user.userId',
+                    to: 'userData.userId'
                 }
             },
             device: {
