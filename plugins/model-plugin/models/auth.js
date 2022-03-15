df018ad476a5350536268f2224f62f23c321b188
Alex Lionne <alexandrelionne27@gmail.com>
2022-03-16 00:43:18 +0100

    revert

--- plugins/model-plugin/models/auth.js
+++ plugins/model-plugin/models/auth.js
@@ -1,44 +1,14 @@
 const {Model} = require('objection')
 const jwt = require('jsonwebtoken');
-const fs = require("fs")
 
 const bcrypt = require("bcryptjs");
 const {config} = require("../config/knex");
-const path = require("path");
 const guid = require('objection-guid')({
     field: 'authId',
 });
-const privateKey = `-----BEGIN RSA PRIVATE KEY-----
-MIIEowIBAAKCAQEAnipQBs5Zik7WX6gwXoIcRcW6JUo42g60vpD9OYkWa/hKL4fI
-fXdvgwLb4PpM4qiAnzBkN7SN1nDwy8nzl/nBbbwFR32xAP+5jW5tbZ9lFvWivkwr
-dk327ePisjAz6bup36ZYUwgXWifto/WobMQ2vTd+VPVnt+tA4wqq7++uZ47W4aGQ
-tCeKNa5It3jKyhdsB5GFvFzEKUnlxUZGTbx4aYyRKffg4J2/McXumjSOOt9DLqxJ
-ee1vnqPNxZtqrug9sC+0L0iMQelgTVUUfxwTz/m9+T7R1oYYq6Xt1U8DQnpu5/bx
-b8Wti7NVqX6bDjjkND5mOB4NyzaHULqahzWjnwIDAQABAoIBADQXuY3UTtdnyeFt
-kqi977PnQLzhakIfj9YoCM73YMQk3cIz1YvFV5vtyB+cMNCCMQK9PHTAaerT/esQ
-08MdRmDvCUoXksHCeAml8jKlyfS66GQG0FXBre+CG/sWNzLnVTbNIdAPHSnbdsCG
-u3c/VMwTR++ReByyRgfBTrWB2o5Oj9hbd7RcJOlHkZRhRfXAUii1TkhsEplP9EJj
-gXbCl8we6bmKXiCtiz+jaVyaUCeDPX4Xd5mce7/9QVwoJ+AgUpwCAJhk95POdf5b
-DvBRgGZgM4eG1jbqTGlydjc9cp/YiE6xLEzApIHzumvsVagQgpXKmWW27699OHpL
-1cHgeRkCgYEAykeA3SUcXn1iFVcQaV9Ymy60fYffHaYo22SmGNAG4lDNH97fClau
-Bxr8VuekByLm5I0n8/NpL5M5166mHhf6e7/4akwkpWgFeEY/HYqaY5E6o7gYspIO
-FcnzHA6zkLNjiSLZj9IthMFSVgbjBaI4Ca1eLLHepfMse3vKaPk8bh0CgYEAyCuZ
-kRt0/sDX1Mf4xeg+2LXDjoMMPniRKLYjxGJApq4zQhW2syLLCWZNMS3CV8w0CaZC
-7eowEykQZtCfK7vYysUrH/gRDnFa4JTrg5BXykSA3KgK7hYsO7fbjQzVtLL88iMr
-4uQ1w9SWb4oMY9SDMDNX1NotdOKMY5QVZeWFm+sCgYAuiCxYswTHp8g8aH7Z/pj/
-ecsDZZIp2+NiNNEQvCoZPcUyI67jTfVSpR6PT1IoYhsDUIV0VPZJf3C68O491f5F
-IXAf48w6UKeRd3dlcFD7vpjiuKLV0Ut0L7TBK5CbJOksztuFfjACbgZmy8bFsP97
-GmqjlXdGbZ45+cwiQdNo8QKBgDBJHC09w1YkrKu+ZUtQP8DE79xzzkNPkCyS60PX
-M6NBiEnbtQu3S+UDkYFYeXX2Y2MkgSalBF3K3RMAJjVFhgE72LBIhczHGVXtdnZx
-HkxDhBY2TxSpspfhPQvkWbTSLGQ4icCcrj+EC5nyYhS+0bqR2BEXZDm64ISw1wyS
-dk1rAoGBAJBRz5GAsLhwyx/925KKW/yNsyahxmOArzlbVtYWVoV8CCdvDYpbrhvc
-OU8Z1A2Rrk5WUaFA8ivKHr3BItR73AhXDNODTvPKXroK+WxMLvTLiVUGFk1odoPf
-4ypQnYMfScD1Z2mWDo6VcZklyBe7P1ieNRwlCE+25Lc9kXxxLtib
------END RSA PRIVATE KEY-----`
 
 const RECOMMENDED_ROUNDS = 12
 const BCRYPT_HASH_REGEX = /^\$2[ayb]\$[0-9]{2}\$[A-Za-z0-9./]{53}$/
-
 
 const options = {
     allowEmptyPassword: false,
@@ -77,8 +47,6 @@ class Auth extends guid(Model) {
 
     // Compares a password to a bcrypt hash, returns whether or not the password was verified.
     async verifyPassword(password) {
-        if (!password) return true
-
         return await bcrypt.compare(password, this[options.passwordField])
     }
 
@@ -97,6 +65,11 @@ class Auth extends guid(Model) {
 
             return hash
         }
+
+        // Throw an error if empty passwords are not allowed.
+        if (!options.allowEmptyPassword) {
+            throw new Error('password must not be empty')
+        }
     }
 
     /* Detect rehashing to avoid undesired effects.
@@ -106,15 +79,12 @@ class Auth extends guid(Model) {
     }
 
     generateJWT() {
-        const self = this
-
         return jwt.sign(
             {
-                authId: self.authId,
+                authId: this.authId,
             },
-            privateKey,
-            {algorithm: 'RS256'}
-        );
+            '6716778962',
+            {expiresIn: '7d',});
     }
 }
 
--- plugins/model-plugin/models/token.js
+++ plugins/model-plugin/models/token.js
@@ -2,23 +2,11 @@ const {Model} = require('objection');
 const {config} = require("../config/knex");
 const jwt = require('jsonwebtoken');
 const User = require("./user");
-const fs = require("fs");
-const path = require("path");
 
 const guid = require('objection-guid')({
     field: 'userTokenId',
 });
-const publicKey = `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnipQBs5Zik7WX6gwXoIc
-RcW6JUo42g60vpD9OYkWa/hKL4fIfXdvgwLb4PpM4qiAnzBkN7SN1nDwy8nzl/nB
-bbwFR32xAP+5jW5tbZ9lFvWivkwrdk327ePisjAz6bup36ZYUwgXWifto/WobMQ2
-vTd+VPVnt+tA4wqq7++uZ47W4aGQtCeKNa5It3jKyhdsB5GFvFzEKUnlxUZGTbx4
-aYyRKffg4J2/McXumjSOOt9DLqxJee1vnqPNxZtqrug9sC+0L0iMQelgTVUUfxwT
-z/m9+T7R1oYYq6Xt1U8DQnpu5/bxb8Wti7NVqX6bDjjkND5mOB4NyzaHULqahzWj
-nwIDAQAB
------END PUBLIC KEY-----`
 
-const SECRET_KEY = process.env.SECRET_KEY
 
 Model.knex(config)
 
@@ -59,24 +47,14 @@ class Token extends guid(Model) {
             if (!token)
                 return false
 
-            return await new Promise((resolve) => {
-                jwt.verify(token, publicKey, {algorithms: ['RS256']}, async function (err, decoded) {
-                    if (err) {
-                        console.error(err)
-                        resolve(false)
-                    }
-                    console.log(decoded)
-                    const authId = decoded.authId
+            const {authId} = jwt.verify(token, '6716778962');
 
-                    if (!authId)
-                        resolve(false)
+            if (!authId)
+                return false
 
-                    const {userId} = await User.query().where('authId', '=', decoded.authId).first()
+            const {userId} = await User.query().where('authId', '=', authId).first()
 
-                    resolve(userId)
-                });
-            })
-
+            return userId
         } catch (e) {
             console.log(e)
             return false
