b56bfc37ef2c492d6561d400efcb827a1668e514
Alexandre Lionne <alexandre@nesga.fr>
2022-11-08 20:35:39 +0100

    test

--- plugins/utils/bucket.js
+++ plugins/utils/bucket.js
@@ -1,7 +1,4 @@
-
-
-
 module.exports = {
-    BUCKET_RESOURCE_OPTIONS : 1000 * 60 * 60 * 3 ,
-    AVATARS_BUCKET : 'all-avatars-bucket'
-};
+  BUCKET_RESOURCE_OPTIONS: 1000 * 60 * 60 * 3,
+  AVATARS_BUCKET: 'all-avatars-bucket'
+}
